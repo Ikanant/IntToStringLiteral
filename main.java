82859f6b0eb54bd8fdca9f70ebafebc4b28bcd3f
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2016-03-09 13:44:19 -0500

    Clean start

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class main{
+  public static void main(String[] args){
+    System.out.println("Input: Integer values from -999 to 999");
+    System.out.print("> ");
+
+    int val;
+    Scanner input = new Scanner(System.in);
+    val = input.nextInt();
+
+    System.out.println(val);
+  }
+}
